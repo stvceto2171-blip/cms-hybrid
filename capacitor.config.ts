04ecad0aeb4f658543e948c06fbc77df37253d45
stvceto2171-blip <stvceto2171@gmail.com>
2025-12-25 22:08:46 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CMS Hybrid',
+  webDir: 'dist'
+};
+
+export default config;
